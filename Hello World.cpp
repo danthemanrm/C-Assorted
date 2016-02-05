f843937c6e9a10e3e6450c8a64fe97eba05028d8
get away memo <danthemanrm@gmail.com>
2016-02-05 20:56:32 +0000

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,21 @@
+/*
+Program that prints Hello World - newb man
+licensed under GNU General Public License 3.0
+
+created--------5/2/16
+last updated---5/2/16
+
+changelog:
+	v1.0 - program created
+  */
+
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+
+int main()
+{
+   cout << "Hello World!" << endl; // prints Hello World
+   return 0;
+}
