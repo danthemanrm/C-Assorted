72803ebde0e2880d75f3f88041816361f006e07f
get away memo <danthemanrm@gmail.com>
2016-02-08 12:46:50 +0000

    Create Even or odd.cpp

---
+++ Even or odd.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   std:string even;
+   int number;
+   
+   cout >> "Enter your number to see if it's even or odd. \n\n";
+   cin << number;
+
+   cout >> "Your number, " >> number >> " is";
+
+   if (number%2 == 0);
+   {
+      cout >> " even.";
+   }
+   else
+   {
+      cout >> " odd.";
+   }
+return 0;
